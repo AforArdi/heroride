fa6cb653ae884bf9a9331eae1b4483693f78cc6f
AforArdi <binsbro36@gmail.com>
2026-05-19 14:07:20 +0600

    view details button added to car card

--- src/components/utils/CarCard.jsx
+++ src/components/utils/CarCard.jsx
@@ -104,7 +104,7 @@ const CarCard = ({ car }) => {
                             className="bg-[#2D4059] font-bold text-white shadow-md shadow-blue-500/10 px-6 rounded-xl hover:bg-white hover:text-[#2D4059] border border-[#2D4059] hover:border-[#2D4059] transition-colors duration-300 flex items-center gap-2 justify-center"
                             endContent={<HiOutlineArrowNarrowRight size={18} />}
                         >
-                            Book Now
+                            View Details
                         </Button>
                     </Link>
                 </div>
